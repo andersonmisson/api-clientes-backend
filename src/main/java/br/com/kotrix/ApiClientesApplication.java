4c5b04474481f51bd2a09ed63ae3a74a8df2b786
Anderson Misson <missondeveloper@gmail.com>
2020-08-05 11:32:33 -0300

    Testando o Banco De Dados

--- src/main/java/br/com/kotrix/ApiClientesApplication.java
+++ src/main/java/br/com/kotrix/ApiClientesApplication.java
@@ -1,10 +1,31 @@
 package br.com.kotrix;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.kotrix.model.entity.Cliente;
+import br.com.kotrix.model.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ApiClientesApplication {
+	
+	@Bean
+	public CommandLineRunner run ( @Autowired ClienteRepository repository ) {
+		return args->{
+			
+			Cliente c1 = new Cliente (null, "Fulano", "00000000000", LocalDate.now());
+			Cliente c2 = new Cliente (null, "Ciclano", "11111111111", LocalDate.now());
+			
+			repository.save(c1);
+			repository.save(c2);
+			
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiClientesApplication.class, args);
